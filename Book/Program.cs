89b79b02970036ef17af832993767bb12b2f65f0
LaprinaME <LaprinaME@206D-2>
2023-03-16 15:34:05 +0700

    Добавьте файлы проекта.

---
+++ Book/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Check();
+        }
+        public class Book
+        {
+            public string Title { get; set; }
+        }
+        public static void Check()
+        {
+            var book = new Book();
+            book.Title = "Structure and interpretation of computer programs";
+            Console.WriteLine(book.Title);
+        }
+    }
+}
